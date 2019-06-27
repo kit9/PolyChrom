5828cb65342a02e9a57abaad45086b9d0dce0568
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-06-27 00:56:11 -0500

    Update __init__.py

--- sticker_management/models/__init__.py
+++ sticker_management/models/__init__.py
@@ -3,9 +3,9 @@
 # © 2017 Niboo SPRL (https://www.niboo.be/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import bartender_host
-from . import print_operation
-from . import stock_picking_type
-from . import mrp_production
+#from . import bartender_host
+#from . import print_operation
+#from . import stock_picking_type
+#from . import mrp_production
 from . import stock_pack_operation
-from . import printed_stickers
+#from . import printed_stickers
