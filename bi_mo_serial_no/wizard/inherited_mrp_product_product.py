eb88a6f2bdc5a5684f0e7920f5928a35c9ac1196
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-09-16 17:19:58 -0500

    Update inherited_mrp_product_product.py

--- bi_mo_serial_no/wizard/inherited_mrp_product_product.py
+++ bi_mo_serial_no/wizard/inherited_mrp_product_product.py
@@ -61,7 +61,7 @@ class MrpProductProduce(models.TransientModel):
 				_logger.info('*** Product Line Lot Name: %s', product_line.lot_id.name)
 				lot_no = prefix+product_line.lot_id.name
 				serialExists = self.env['stock.production.lot'].search(['&', ('name', '=', lot_no), ('product_id', '=', self.product_id.id)])
-				_logger.info('*** Serial Exists: %s', serialExistse)
+				_logger.info('*** Serial Exists: %s', serialExists)
 				if serialExists == False:
 					_logger.info('*** Creating item: %s', lot_no)
 					lot_serial_no = self.env['stock.production.lot'].create({'name' : lot_no,'product_id':self.product_id.id})
