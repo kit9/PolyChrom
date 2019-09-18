497499d1c7e5b4110a74292d50ce63591b27cfc0
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-09-18 02:33:49 -0500

    Update inherited_res_company.py

--- bi_mo_serial_no/models/inherited_res_company.py
+++ bi_mo_serial_no/models/inherited_res_company.py
@@ -117,12 +117,12 @@ class MrpProductionInherit(models.Model):
     	def action_assign(self):
         	for production in self:
             		production.move_raw_ids._action_assign()
-			if production.bom_id.prev_product_id:
-				line = production.move_raw_ids.search(['&', ('product_id', '=', production.bom_id.prev_product_id.id), ('production_id', '=', production.id)], limit=1)
-				work_order = production.workorder_ids.search(['&', ('product_id', '=', production.bom_id.prev_product_id.id), ('production_id', '=', production.id)], limit=1)
-				for lot in lin.active_move_line_ids:
-					lot_line = work_order.active_move_line_ids.search(['&', ('lot_id', '=', False), ('product_id', '=', work_order.product_id), ('work_order_id', '=', work_order.id)], limit=1)
-					lot_line.lot_id = line.lot_id
+			#if production.bom_id.prev_product_id:
+			#	line = production.move_raw_ids.search(['&', ('product_id', '=', production.bom_id.prev_product_id.id), ('production_id', '=', production.id)], limit=1)
+			#	work_order = production.workorder_ids.search(['&', ('product_id', '=', production.bom_id.prev_product_id.id), ('production_id', '=', production.id)], limit=1)
+			#	for lot in lin.active_move_line_ids:
+			#		lot_line = work_order.active_move_line_ids.search(['&', ('lot_id', '=', False), ('product_id', '=', work_order.product_id), ('work_order_id', '=', work_order.id)], limit=1)
+			#		lot_line.lot_id = line.lot_id
         	return True
 
 class MrpworkorderInherit(models.Model):
