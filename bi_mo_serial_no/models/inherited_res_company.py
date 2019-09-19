e1333267d99cfd88045e446ce3fb0ab77fb0f87d
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-09-19 00:45:32 -0500

    Update inherited_res_company.py

--- bi_mo_serial_no/models/inherited_res_company.py
+++ bi_mo_serial_no/models/inherited_res_company.py
@@ -269,11 +269,18 @@ class MrpworkorderInherit(models.Model):
 			self._generate_lot_ids()
 
 		self.final_lot_id = int(self.lot_numbr)
-		move = self.production_id.move_raw_ids.filtered(lambda move: move.workorder_id.id == self.id and (move.product_id.id == self.production_id.bom_id.prev_product_id.id or move.product_id.tracking == 'lot'))
-		self.current_quality_check_id.update({'lot_id': move.active_move_line_ids[0].lot_id.id})
-
+		
 		if self.next_work_order_id and self.production_id.product_id.tracking != 'none':
 			self.next_work_order_id._assign_default_final_lot_id()
+		
+		move = self.production_id.move_raw_ids.filtered(lambda move: move.workorder_id.id == self.id and (move.product_id.id == self.production_id.bom_id.prev_product_id.id or move.product_id.tracking == 'lot'))
+		if self.production_id.product_id.tracking == 'lot':
+			self.current_quality_check_id.update({'lot_id': move[0].active_move_line_ids[0].lot_id.id})
+		elif self.production_id.product_id.tracking == 'serial':
+			prefix = self.production_id.product_id.prefix_serial_no
+			new_lot = move[0].active_move_line_ids.filtered(lambda lot: prefix+lot.lot_id.name == self.final_lot_id.name)
+			#self.env['stock.production.lot'].search([('name', '=', self.production_id.product_id.name)])
+			self.current_quality_check_id.update({'lot_id': new_lot[0].lot_id.id)
 
 		if float_compare(self.qty_produced, self.production_id.product_qty, precision_rounding=rounding) >= 0:
 			self.button_finish()
