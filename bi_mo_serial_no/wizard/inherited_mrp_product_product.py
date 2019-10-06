811cd8e3d9168134e6ca9e8fe9dbbccea1dce80f
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-10-05 23:03:14 -0500

    Update inherited_mrp_product_product.py

--- bi_mo_serial_no/wizard/inherited_mrp_product_product.py
+++ bi_mo_serial_no/wizard/inherited_mrp_product_product.py
@@ -28,45 +28,29 @@ class MrpProductProduce(models.TransientModel):
 		if 'produce_line_ids' not in fields:
 			fields.append('produce_line_ids')
 			
-		_logger.info('*** Self Context: %s', self._context)
 		
 		res = super(MrpProductProduce, self).default_get(fields)
-		_logger.info('*** Fields are: %s', fields)
 		if 'production_id' in res:
 			production = self.env['mrp.production'].browse(res['production_id'])
 			lot_serial_no = False
-			_logger.info('*** Production: %s', production)
-			_logger.info('*** BOM: %s', production.bom_id)
-			_logger.info('*** Prev Prod: %s', production.bom_id.prev_product_id)
-			_logger.info('*** Prod: %s', production.product_id)
-			_logger.info('*** tracking: %s', production.product_id.tracking)
-			_logger.info('*** Self Line Ids: %s', self.produce_line_ids)
 			if production and production.bom_id and production.bom_id.prev_product_id:
 				prefix = production.product_id.prefix_serial_no
 				prev_prod = production.bom_id.prev_product_id.id
 				move = production.move_raw_ids.filtered(lambda x: x.product_id.id == prev_prod and x.product_id.tracking != 'none' and x.state not in ('done', 'cancel') and x.bom_line_id)
-				#product_line = self.produce_line_ids.search(['&', ('product_produce_id', '=', self.id), ('product_id', '=', prev_prod)], limit=1)
-				#if product_line:
+				
 				if move:
 					move_line = move[0].move_line_ids.filtered(lambda x: not x.lot_produced_id)[0]
 					lot_no = prefix+move_line.lot_id.name
-					_logger.info('*** Looking for serial: %s', lot_no)
-				#	lot_no = prefix+product_line.lot_id.name
 					serialExists = self.env['stock.production.lot'].search(['&', ('name', '=', lot_no), ('product_id', '=', production.product_id.id)])
 					if not serialExists:
-						_logger.info('*** Serial Not Exists: %s', serialExists)
 						lot_serial_no = self.env['stock.production.lot'].create({'name' : lot_no,'product_id':production.product_id.id})
 					else:
-						_logger.info('*** Serial Does Exist: %s as name: %s', serialExists, serialExists[0].name)
 						lot_serial_no = serialExists[0]
 			elif production.product_id.tracking != 'none':
-				_logger.info('*** Here is your tracking: %s', production.product_id.tracking)
 				lot_serial_no = production.create_custom_lot_no()
 			if lot_serial_no:
-				_logger.info('*** Set the lot_id: %s', lot_serial_no)
 				res['lot_id'] = lot_serial_no.id
-				#production.create_custom_lot_no().id
-		_logger.info('*** Res Fields are: %s', res)
+		
 		return res
 	
 	@api.multi
@@ -86,7 +70,6 @@ class MrpProductProduce(models.TransientModel):
 
 	@api.multi
 	def do_produce(self):
-
 		company = self.env['res.company']._company_default_get('mrp.product.produce')
 		result = self.env['res.config.settings'].search([],order="id desc", limit=1)
 
@@ -100,7 +83,6 @@ class MrpProductProduce(models.TransientModel):
 		serial_no = company.serial_no + 1
 		serial_no_digit=len(str(company.serial_no))
 
-		
 		diffrence = abs(serial_no_digit - digit)
 		if diffrence > 0:
 			no = "0"
@@ -115,7 +97,6 @@ class MrpProductProduce(models.TransientModel):
 			raise UserError(_("The production order for '%s' has no quantity specified") % self.product_id.display_name)
 		for move in self.production_id.move_raw_ids:
 			# TODO currently not possible to guess if the user updated quantity by hand or automatically by the produce wizard.
-			_logger.info('*** Important If Values: (%s, %s, %s)', move.product_id.tracking, move.state, move.unit_factor)
 			if move.product_id.tracking == 'none' and move.state not in ('done', 'cancel') and move.unit_factor:
 				rounding = move.product_uom.rounding
 				if self.product_id.tracking != 'none':
